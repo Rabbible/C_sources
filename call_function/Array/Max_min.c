e8f2e8711d02ea0999581fd2c6fc786cd3ff4f0f
Kegence <1687710153@qq.com>
2021-11-18 15:32:15 +0800

    update

--- call_function/Array/Max_min.c
+++ call_function/Array/Max_min.c
@@ -1,26 +1,32 @@
+//头文件声明
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+//函数声明
 double average(int a[],int n);
 int max(int a[],int n);
 int min(int a[],int n);
+//主函数
 int main()
 {
     int a[10],i,j,k;
     double m;
     srand((unsigned)time(NULL));
+    //随机时间
     for(i=0;i<10;i++)
     {
         a[i]=rand()%100;
         printf("a[%d]=%d  ",i,a[i]);
     }
     printf("\n");
+    //调用函数
     m=average(a,10);
     j=max(a,10);
     k=min(a,10);
     printf("average=%lf\nmax=%d\nmin=%d",m,j,k);
     return 0;
 }
+//求平均函数
 double average(int a[],int n)
 {
     int i,sum=0;
@@ -30,6 +36,7 @@ double average(int a[],int n)
     }   
     return (double)sum/10;
 }
+// 求最大值函数
 int max(int a[],int n)
 {
     int max,i;
@@ -43,6 +50,7 @@ int max(int a[],int n)
     }
     return max;
 }
+// 求最小值函数
 int min(int a[],int n)
 {
     int min,i;
